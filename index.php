abd09e831d0c343f4bc714f0651acce2b62db793
Rainer T <rainer.tahker@tptlive.ee>
2026-09-03 09:39:21 +0300

    Add Comments Example

--- index.php
+++ index.php
@@ -4,5 +4,11 @@
  $test = 'Hello world!';
  $test = '❤️🙌';
  $test = NuLl;
+ $test = [1, null, true, 'lol', 2, 3.2];
+ $test = new stdClass();
+ $test->name = 'Rainer';
  var_dump($test);
+
+ //Comment
+  /*Nigeros pagareros*/
 ?>
