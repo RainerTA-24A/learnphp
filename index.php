776403ba321f815bd2f33876a612594093ae6a6d
Rainer T <rainer.tahker@tptlive.ee>
2026-09-03 09:52:00 +0300

    Add Maths Example

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php
- $test = 10;
- $test = 4.8;
- $test = 'Hello world!';
- $test = '❤️🙌';
- $test = NuLl;
- $test = [1, null, true, 'lol', 2, 3.2];
- $test = new stdClass();
- $test->name = 'Rainer';
- var_dump($test);
-
- //Comment
-  /*Nigeros pagareros*/
-?>
+$test = 10;
+$test = 10 + 10;
+$test = 10 - 10;
+$test = 10 * 10;
+$test = $test + 10;
+$test = 2 ** 2;
+$test = 5 % 2;
+$test += 3;
+$test -= 10;
+$test *= 10;
+$test /= 10;
+$test %= 10;
+$test **= 10;
+$test = 10;
+$test++;
+$test--;
+--$test;
+++$test;
+ 
+$a = 1;
+$b = ++$a;
+var_dump($b, $a);
+ 
+$test = sin(pi());
+var_dump($test);
