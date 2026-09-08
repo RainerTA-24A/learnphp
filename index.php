ca1358778ce7a63e1ae37dba5922f138258d7c1a
Rainer T <rainer.tahker@tptlive.ee>
2026-09-08 09:25:23 +0300

    Add array example

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 == 10;
-$test = 10 <= 10;
-$test = 10 >= 10;
-$test = 10 == '10';
-$test = 10 ===  '10';
-$test = 10 != '10';
-$test = 10 !== '10';
-$test = [] == false;
-var_dump( $test );
-//https://www.php.net/manual/en/types.comparisons.php
+$test = array(1, 2, 3); //vana viis
+$test = [1, 2, 3]; //uus viis
+$test = [1,'asdas', false, [1,2,3]];
+$test = [
+    1,
+    'name' => 'Rainer',
+    'age'=> 18,
+    3,
+    'aadsas',
+    true,
+    100 => 'lol',
+    'troll',
+];
+
+
+
+
+var_dump($test['101']);
+array_push($test, 'added', 'shimishimiaa', 3);
+$test[] = 'new valus';
+$test['name'] = 'Mats';
+$test[1] = 9999;
+var_dump($test);
+unset($test[3]);
 ?>
