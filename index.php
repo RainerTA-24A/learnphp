e934b5894fa65aa01b8bcd2a132a0f5619a6322f
Rainer T <rainer.tahker@tptlive.ee>
2026-09-08 09:41:41 +0300

    Add if and switch case examples

--- index.php
+++ index.php
@@ -1,26 +1,65 @@
 <?php
-$test = array(1, 2, 3); //vana viis
-$test = [1, 2, 3]; //uus viis
-$test = [1,'asdas', false, [1,2,3]];
-$test = [
-    1,
-    'name' => 'Rainer',
-    'age'=> 18,
-    3,
-    'aadsas',
-    true,
-    100 => 'lol',
-    'troll',
-];
+$num = 10;
+if($num > 10){
+    var_dump('Bigger');
+}
+if($num < 10){
+    var_dump('Smaller');
+} else {
+    var_dump('Bigger');
+}
+if($num > 10) {
+    var_dump('Bigger');
+} else if($num < 10) {
+    var_dump('Smaller');
+} else {
+    var_dump('Equal');
+}
 
+$day = (int) date('w');
+$day = intval(date('w'));
+var_dump($day);
+if($day === 0) {
+    var_dump('Sunday');
+} elseif($day === 1) {
+    var_dump('Monday');
+} elseif($day === 2) {
+    var_dump('Tuesday');
+} elseif($day === 3) {
+    var_dump('Wednesday');
+} elseif($day === 4) {
+    var_dump('Thursday');
+} elseif($day === 5) {
+    var_dump('Friday');
+} elseif($day === 6) {
+    var_dump('Saturday');
+} else{
+    var_dump('WeirdLols');
+}
 
-
-
-var_dump($test['101']);
-array_push($test, 'added', 'shimishimiaa', 3);
-$test[] = 'new valus';
-$test['name'] = 'Mats';
-$test[1] = 9999;
-var_dump($test);
-unset($test[3]);
+switch($day) {
+    case 0:
+        var_dump('Sunday');
+        break;
+    case 1:
+        var_dump('Monday');
+        break;
+    case 2:
+        var_dump('Tuesday');
+        break;
+    case 3:
+        var_dump('Wednesday');
+        break;
+    case 4:
+        var_dump('Thursday');
+        break;
+    case 5:
+        var_dump('Friday');
+        break;
+    case 6:
+        var_dump('Saturday');
+        break;
+    default:
+        var_dump('WeirdLols');
+}
 ?>
