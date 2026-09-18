b8ec2e77493ee3a718f6ecd61bb9fa9581c0babc
Rainer T <rainer.tahker@tptlive.ee>
2026-09-18 09:31:10 +0300

    a little better ...

--- index.php
+++ index.php
@@ -13,13 +13,13 @@ $name = 'Rainer';
     <h1>Hello <?php echo $name?>!</h1>
     <h1>Hetri <?= $name?>!</h1>
     <ul>
-        <?php for($i=0; $i<10;$i++) { ?>
-            <?php if($i%2 === 0) { ?>
+        <?php for($i=0; $i<10;$i++):  ?>
+            <?php if($i%2 === 0):  ?>
             <li style="color:green"> <?= $i ?></li>
-            <?php } else { ?>
+            <?php  else:  ?>
             <li style="color:red"> <?= $i ?></li>
-            <?php } ?>
-        <?php } ?>
+            <?php endif ?>
+        <?php endfor ?>
     </ul>
 </body>
 </html>
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 php -S localhost:8000
 ./server.sh
 sh server.sh
---3 viisi ig
+--3 viisi ig    
